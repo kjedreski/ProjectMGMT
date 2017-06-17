483def2182915c60583fb84c02988cc0b434922f
Kevin Jedreski <Kevin Jedreski>
2017-06-17 13:59:56 -0400

    initalize all begining files and json package

---
+++ app/index.js
@@ -0,0 +1,17 @@
+const electron = module.require('electron')
+const {app} = electron
+const {BrowserWindow} = electron
+
+
+app.on('ready', function() {
+    
+    // will run a window when app starts
+    let mainWindow = new BrowserWindow( {
+        width: 300,
+        height: 500,
+        x: 100,
+        y: 100,
+        transparent: true
+    })
+
+})
