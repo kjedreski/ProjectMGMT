f4c5ee8573aa209f2cb5f24f27ec45c721236ba2
Kevin Jedreski <Kevin Jedreski>
2017-06-18 20:45:21 -0400

    starting to implement data storage, electron-storage, working on reading JSON object in main

--- app/scripts/Config.js
+++ app/scripts/Config.js
@@ -1,8 +1,26 @@
-
-// Add task
+// Add tas
+const storage = require('electron-json-storage');
+const remote = require('electron').remote;
+const app = remote.app;
 
 
  function AddTask() {
+     alert("in fn")
+     //alert("path: " + app.getPath('userData'))
+     let TaskName = document.getElementsByName("TaskName")[0].value
+     storage.set('Tasks', {taskName: TaskName }, function(error) {
+         if (error) throw error;
+     } )
+    //alert(document.getElementsByName("TaskName")[0].value )
+}
 
-    alert(document.getElementsByName("TaskName").valueOf())
+
+function LoadTasks() {
+    
+storage.getAll(function(error, data) {
+  if (error) throw error;
+  alert(data["taskName"].value)
+  console.log(data);
+})
+    
 }
